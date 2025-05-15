3b38d22f0407775b095c0f962ffb6fbf7070ec46
Bilal Ahmed <Syedbilalahmed2004@gmail.com>
2025-05-15 14:31:35 +0500

    Testimonials Image Case Sensitivity Issue Resolve

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -20,8 +20,8 @@ import Box2 from '../assets/HowItWorks/Box2.svg'
 import Box3 from '../assets/HowItWorks/Box3.svg'
 import Box4 from '../assets/HowItWorks/Box4.svg'
 
-import MaleAvatar from '../assets/testimonial/Male.svg'
-import FemaleAvatar from '../assets/testimonial/Female.svg'
+import MaleAvatar from '../assets/Testimonial/Male.svg'
+import FemaleAvatar from '../assets/Testimonial/Female.svg'
 
 // Social media icons
 import FacebookIcon from '../assets/socials/facebook.svg'
